8fbcd7fa8d843abd6610cda8b34d0377a7403bf4
elparduh <ticantevicentejuan@outlook.com>
2024-09-25 23:23:04 -0600

    create view in switui

--- Meow/ContentView.swift
+++ Meow/ContentView.swift
@@ -2,13 +2,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image(.haku)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 64.0))
+                    .padding()
+                Button(action: {}) {
+                    Text("Generar imagen")
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(.black, lineWidth: 1)
+                        )
+                        .foregroundColor(.black)
+                }
+            }
+            .navigationTitle("Meow")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
