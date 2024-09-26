bcde19c3f17d91d94e8b19513b560047f8a8191b
elparduh <ticantevicentejuan@outlook.com>
2024-09-25 22:20:06 -0600

    Initial Commit

---
+++ Meow/MeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowApp.swift
+//  Meow
+//
+//  Created by Vicente Juan on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
