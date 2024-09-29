f2c455ee16bb4f3d97c0d51ee4cbeb0d12bad7c7
elparduh <ticantevicentejuan@outlook.com>
2024-09-29 07:44:49 -0600

    add HttpRequestStatus

--- Meow/core/network/HttpRequestStatus.swift
+++ Meow/core/network/HttpRequestStatus.swift
@@ -10,3 +10,19 @@ enum HttpRequestStatus {
     case connectionNetwork
     case unknown(String)
 }
+
+extension HttpRequestStatus {
+    
+    func getMessage() -> String {
+        switch self {
+        case .successfulRequest: "Success";
+        case .badRequest: "Bad request";
+        case .unauthorizedRequest: "User unauthorized";
+        case .forbiddenRequest: "Forbidden request"
+        case .internalServerError: "Some thing went wrong"
+        case .notFoundError: "Url not found"
+        case .connectionNetwork: "No internet connection"
+        case .unknown(let msg): msg
+        }
+    }
+}
