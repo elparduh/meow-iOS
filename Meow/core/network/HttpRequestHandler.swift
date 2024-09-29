eac1bcd914293807b336955cb4d3144d929a1dec
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2024-09-29 13:37:24 -0600

    Create HttpRequestHandler - Core (#2)

---
+++ Meow/core/network/HttpRequestHandler.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+struct HttpRequestHandler {
+    
+    func createRequestStatus(response: (data: Data, httpResponse: URLResponse)) -> HttpRequestStatus {
+        let httpResponse = response.httpResponse as? HTTPURLResponse
+        
+        switch httpResponse?.statusCode {
+        case 200: return HttpRequestStatus.successfulRequest
+        case 400: return HttpRequestStatus.badRequest
+        case 401: return HttpRequestStatus.unauthorizedRequest
+        case 403: return HttpRequestStatus.forbiddenRequest
+        case 404: return HttpRequestStatus.notFoundError
+        case 500: return HttpRequestStatus.internalServerError
+        default:
+            let serverData = String(data: response.data, encoding: String.Encoding.utf8)
+            return HttpRequestStatus.unknown(serverData ?? "Unknown");
+        }
+    }
+    
+    func createRequestStatusFrom(error: Error) -> HttpRequestStatus {
+        if error.isNetworkConnectionError() {
+            return HttpRequestStatus.connectionNetwork
+        }
+        return HttpRequestStatus.unknown(error.localizedDescription)
+    }
+}
