a4776314d74e5246ca0aa7bffec52cd9cef93746
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2024-11-24 05:39:31 -0600

    add func

--- Meow/core/features/data/remote/CatImageRemoteDataSource.swift
+++ Meow/core/features/data/remote/CatImageRemoteDataSource.swift
@@ -1,2 +1,20 @@
 import Foundation
 
+protocol CatImageRemoteDataSourceProtocol {
+  func fetchCatImage() async throws -> CatImage
+}
+
+struct CatImageRemoteDataSource: CatImageRemoteDataSourceProtocol {
+
+  private let httpRequestHandler: HttpRequestHandler
+  private let urlSession: URLSession
+
+  init(httpRequestHandler: HttpRequestHandler, urlSession: URLSession) {
+    self.httpRequestHandler = httpRequestHandler
+    self.urlSession = urlSession
+  }
+
+  func fetchCatImage() async throws -> CatImage {
+    return CatImage()
+  }
+}
