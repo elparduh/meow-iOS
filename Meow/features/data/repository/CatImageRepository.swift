a3d78731ab3074dc255768d2b9edbb3a5c45b320
parduh-mercanto <juan@mercanto.mx>
2024-11-24 19:05:01 -0600

    add improvs

---
+++ Meow/features/data/repository/CatImageRepository.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+protocol CatImageRepositoryProtocol {
+    var catImageRemoteDataSourceProtocol: CatImageRemoteDataSourceProtocol { get }
+    func fetchCatImage() async throws -> CatImage
+}
+
+struct CatImageRepository: CatImageRepositoryProtocol {
+    var catImageRemoteDataSourceProtocol: CatImageRemoteDataSourceProtocol
+    
+    init(catImageRemoteDataSourceProtocol: CatImageRemoteDataSourceProtocol) {
+        self.catImageRemoteDataSourceProtocol = catImageRemoteDataSourceProtocol
+    }
+    
+    func fetchCatImage() async throws -> CatImage {
+        try await catImageRemoteDataSourceProtocol.fetchCatImage()
+    }
+}
